faa92553731a0b68f3969c8f7d8f81558bd511ac
SAntonets <141963865+SAntonets@users.noreply.github.com>
2023-12-15 22:59:01 +0200

    task 2 done

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,27 @@
+class Storage {
+    #items;
+
+    constructor(initialItems) {
+        this.#items = initialItems;
+    }
+
+    getItems() {
+        return this.#items;
+    }
+
+    addItem(newItem) {
+        this.#items.push(newItem);
+    }
+
+    removeItem(itemToRemove) {
+        const indexToRemove = this.#items.indexOf(itemToRemove);
+        if (indexToRemove !== -1) {
+      this.#items.splice(indexToRemove, 1);
+    }
+    }
 
 
-
+}
 
 
 // Задача 2. Склад
